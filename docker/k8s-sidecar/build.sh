056c62d49cf0816d4537bd65249c45f46d7d84f5
Pieter van der Merwe <pieterv@icloud.com>
2019-08-24 15:17:12 +0200

    Change sidecar tag Add image override

--- docker/k8s-sidecar/build.sh
+++ docker/k8s-sidecar/build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-echo "Building pietervicloudcom/k8-sidecar-armhf:python3-7-slim-armhf"
-docker buildx build --push --platform linux/arm/v7 -t pietervicloudcom/k8-sidecar-armhf:python3-7-slim-armhf .
+echo "Building pietervicloudcom/k8-sidecar-armhf:0.0.18"
+docker buildx build --push --platform linux/arm/v7 -t pietervicloudcom/k8-sidecar-armhf:0.0.18 .
 
 
