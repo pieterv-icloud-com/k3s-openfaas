f729b605ca213a10125190238d115386f60de639
Pieter van der Merwe <pieterv@icloud.com>
2019-08-25 13:47:34 +0200

    Fix build issue

--- docker/kube-webhook-certgen/.cicd/build.sh
+++ docker/kube-webhook-certgen/.cicd/build.sh
@@ -9,6 +9,7 @@ docker run --rm \
   -e GO111MODULE=on  \
   -e CGO_ENABLED=0   \
   -e GOOS=linux      \
-  -e GOARCH=amd64    \
+  -e GOARCH=arm      \
+  -e GOARM=7         \
   golang:1.12-stretch \
     go build -mod=vendor -ldflags "-X $mod/core.Version=$vers -X $mod/core.BuildTime=$buildTime"
